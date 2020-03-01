c8fcf27b501396d6e0cfed1b169093e81015fb39
adun1 <bmccormack15@gmail.com>
2020-03-01 08:15:42 +0000

    evenNumber

---
+++ funPrac.js
@@ -0,0 +1,5 @@
+function movies(messageFunction, name){
+  messageFunction(name);
+}
+
+movies(function displayFavorite(movieName){console.log("My favorite movie is " + movieName);}, "Finding Nemo");
