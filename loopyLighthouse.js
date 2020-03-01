c8e11e1ca47d610693e7e900abb7c75bb866cfbd
adun1 <bmccormack15@gmail.com>
2020-03-01 06:12:56 +0000

    Loopylighthouse Exercise

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,6 +1,6 @@
 for(let i = 100; i <= 200; i++)
 {
-  if((i % 3 === 0)&&(i % 4 === 0))
+  if((i % 3 === 0) && (i % 4 === 0))
     console.log("LoopyLighthouse");
   else if(i % 3 === 0)
     console.log("Loopy");
